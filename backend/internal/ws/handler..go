ae07bb6dbb13a551c67a827115c0a8270c0e6177
arif <arif-celik56@hotmail.com>
2024-06-29 09:19:06 +0300

    added some bugs to be fixed later

--- backend/internal/ws/handler..go
+++ backend/internal/ws/handler..go
@@ -25,23 +25,36 @@ func NewWSHandler(repo *wsRepo, userRepo *auth.AuthRepo) *wsHandler {
 	return &wsHandler{repo: repo, userRepo: userRepo}
 }
 
+// FIX there is an close error after 15 seconds, idk why. i tried other ws packages, but still the same
+// on client side, it says "close 1006"
 func (c *wsHandler) OnOpen(conn *gws.Conn) {
-	_ = conn.SetDeadline(time.Now().Add(PingInterval + PingWait))
+	err := conn.SetDeadline(time.Now().Add(PingInterval + PingWait))
+	if err != nil {
+		log.Warn("setdeadline", "err", err)
+	}
 }
 
 func (c *wsHandler) OnClose(conn *gws.Conn, err error) {
-	log.Info("connection closed", "err", err)
+	log.Warn("connection closed", "err", err)
 }
 
 func (c *wsHandler) OnPing(conn *gws.Conn, payload []byte) {
-	_ = conn.SetDeadline(time.Now().Add(PingInterval + PingWait))
-	_ = conn.WritePong(nil)
+	err := conn.SetDeadline(time.Now().Add(PingInterval + PingWait))
+	if err != nil {
+		log.Error("setdeadline", "err", err)
+	}
+	err = conn.WritePong(nil)
+	if err != nil {
+		log.Error("write pong", "err", err)
+	}
 }
 
 func (c *wsHandler) OnPong(conn *gws.Conn, payload []byte) {}
 
 func (c *wsHandler) OnMessage(conn *gws.Conn, message *gws.Message) {
 	defer message.Close()
+
+	log.Info("message received", "message", message.Data.String())
 
 	conn.WriteMessage(message.Opcode, []byte("hii"))
 }
