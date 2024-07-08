910c0b1f1b31d32c9df6008ca266659a44e41fe1
arif <arif-celik56@hotmail.com>
2024-07-08 16:11:00 +0300

    migrated from vue to react due to market

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
+import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(), AutoImport({
-      dirs: ['src/components'],
-      imports: ['vue', 'vue-router', '@vueuse/core']
-    })
-  ],
+  plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
