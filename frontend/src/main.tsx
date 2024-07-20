09b8fb5b2aae5e803dd1b1665976d25dc4d874d3
arif <arif-celik56@hotmail.com>
2024-07-20 10:04:13 +0300

    chore: Update import statements to use single quotes in utils.ts and main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/ThemeProvider'
 import './main.css'
 
 import { routeTree } from './routeTree.gen'
@@ -8,13 +10,16 @@ import { routeTree } from './routeTree.gen'
 const router = createRouter({ routeTree })
 
 declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+	interface Register {
+		router: typeof router
+	}
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<RouterProvider router={router} />
+			<Toaster richColors position="top-right" duration={3000} />
+		</ThemeProvider>
+	</React.StrictMode>
 )
