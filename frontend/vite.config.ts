148cb572d7effac7560d7fd2857d3bf7672e1b50
arif <arif-celik56@hotmail.com>
2024-07-20 10:24:36 +0300

    chore: Update import statements to use single quotes in utils.ts and main.tsx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from "node:path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [TanStackRouterVite(), react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src")
+		}
+	}
 })
