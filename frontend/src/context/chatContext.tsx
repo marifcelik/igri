3a527d085311d798768501b643a17898faa8a090
arif <arif-celik56@hotmail.com>
2024-10-09 22:57:05 +0300

    refactor: update ChatContext to include receiver state

--- frontend/src/context/chatContext.tsx
+++ frontend/src/context/chatContext.tsx
@@ -1,15 +1,11 @@
-import { createContext, useState } from 'react'
+import { createContext } from 'react'
 import type { WSMessage } from '@/types'
 
 type ChatContextType = {
 	messageHistory: WSMessage[]
 	setMessageHistory: React.Dispatch<React.SetStateAction<WSMessage[]>>
+	receiver: string | null
+	setReceiver: React.Dispatch<React.SetStateAction<string | null>>
 }
 
 export const ChatContext = createContext<ChatContextType | undefined>(undefined)
-
-export function ChatProvider({ children }: { children: React.ReactNode }) {
-	const [messageHistory, setMessageHistory] = useState<WSMessage[]>([])
-
-	return <ChatContext.Provider value={{ messageHistory, setMessageHistory }}>{children}</ChatContext.Provider>
-}
