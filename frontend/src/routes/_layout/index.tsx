3223f0b6871cd73815819ab06eaecf175b165ddf
arif <arif-celik56@hotmail.com>
2024-11-11 17:34:14 +0300

    changed heders

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useTheme } from '@/components/ThemeProvider'
 import Globe from '@/components/ui/globe'
-import AnimatedGradientText from '@/components/ui/animated-gradient-text/'
+import AnimatedGradientText from '@/components/ui/animated-gradient-text'
 import DotPattern from '@/components/ui/dot-pattern'
 import ShimmerButton from '@/components/ui/shimmer-button'
 
@@ -17,16 +17,18 @@ function Index() {
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-2">
 			<div className="relative flex h-[300px] sm:h-full w-full flex-col items-center justify-center overflow-hidden bg-background">
-				<AnimatedGradientText text="İgri ebi" />
-				<div className="flex mt-5 items-center justify-center">
+				<AnimatedGradientText text="İgri" />
+				<div className="flex flex-col sm:flex-row mt-5 items-center justify-center">
+					<h2 className="sm:w-1/3 text-muted-foreground font-semibold m-5">İgri is a simple chat application</h2>
 					<ShimmerButton onClick={() => navigate({ to: '/home' })} className="shadow-2xl z-20">
 						<span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
-							tee ışrap çeye
+							Login Now
 						</span>
 					</ShimmerButton>
 				</div>
 				<DotPattern className="[mask-image:radial-gradient(300px_circle_at_center,white,transparent)] -z-0" />
 			</div>
+			{/* FIX when page is firstly loaded, globe is moving on mouse hover */}
 			<Globe dark={theme === 'dark' ? 0 : 1} />
 		</div>
 	)
